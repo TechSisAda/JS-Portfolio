26dc88ce2f91b3876f49b7c3eb1f190ae4740d0c
Adaeze Nnonyelu <nnonyeluadaeze@gmail.com>
2025-02-03 05:34:15 +0100

    added files

---
+++ index.js
@@ -0,0 +1,25 @@
+// console.log(`Hello`);
+// console.log(`i like pizza`);
+// alert("ADA ALERT");
+// document.getElementById("myH1").textContent = `Hello world`;
+// document.getElementById(`myP`).textContent= `i like pizza`;
+// let name = `ADA`; 
+// let age = 25;
+// let price = 20.2
+// console.log(`price + age is = `, age + price);
+// console.log(typeof price);
+// console.log(typeof name);
+
+// let online = true;
+// console.log(`bro is online: ${online}`);
+
+// let forsale = false;
+// console.log(`Pineapples are for sale ${forsale}`);
+
+let fullName = 'adaeze kris';
+let age = 25;
+let isStudent = false;
+document.getElementById('P1').textContent = `your name is ${fullName}`;
+document.getElementById('P2').textContent = `you are ${age} years old`;
+document.getElementById('P3').textContent = `Enrolled: ${isStudent}`;
+
